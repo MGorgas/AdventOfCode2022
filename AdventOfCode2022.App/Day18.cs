5f18c64e5f43aeeaf074b768317bc706ee4c4311
Martin Gorgas <martin.gorgas@th-wildau.de>
2022-12-20 16:16:39 +0100

    Day18 finished part 2. 2D slicing still gives an error for the second part and only the input data. Switched to calculations in 3D space

--- AdventOfCode2022.App/Day18.cs
+++ AdventOfCode2022.App/Day18.cs
@@ -1,8 +1,11 @@
 ﻿namespace AdventOfCode2022.App;
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,18 +14,21 @@ public class Day18
 {
 
 	public static string filepath = @"C:\Users\Martin\source\repos\AdventOfCode2022\AdventOfCode2022.App\day18.txt";
+
 	public static void BuilingBoulders()
 	{
 		int[][] droplets = File.ReadLines(filepath).Select(l => l.Split(',')).Select(r => new[] { int.Parse(r[0]), int.Parse(r[1]), int.Parse(r[2]) }).ToArray();
 
+		var dis = droplets.Distinct().ToList();
+
 		/* - imagine a 3d object formed from those droplets
 		 * - slice the object
-		 * - iterate throug the sclices, slice by z(depth)
+		 * - iterate through the slices, slice by z(depth)
 		 * 
 		 * for a plane just take two coordinates of a droplet and put it in the plane
 		 * use only the droplets for the specific depths
 		 * 
-		 * when going throug the object, do it for all of the three dimensions and count the visible faces of this slice
+		 * when going through the object, do it for all of the three dimensions and count the visible faces or edges of this slice
 		 * 
 		 * ^			^
 		 * |   111		|  111
@@ -37,152 +43,290 @@ public class Day18
 		 *  
 		 */
 
+		int part1 = CountEdges(droplets);
+		Console.WriteLine($"Part 1_Slices: {part1}");
+
+		int part2 = CountEdges(droplets, true); // works only for the test data
+		Console.WriteLine($"Part 2_Slices: {part2}"); // wrong results [to low] = 2367, 2370, 2392, 2462  [right should be 2492, maybe come back it later]
 
 
-		// rotate the slices 3 times to iterate throug z, than x and than y
 
-		Console.WriteLine($"Part 1: {CountFaces(droplets)}");
-		Console.WriteLine($"Part 2: {CountFaces(droplets, true)}");
+		// 2nd attempt after reading and learning from maaaaaany solutions
+
+		int part1_2 = CountFaces3D(droplets);
+		Console.WriteLine($"Part 1_3D: {part1_2}");
+
+		int part2_2 = CountFaces3D(droplets, true);
+		Console.WriteLine($"Part 2_3D: {part2_2}");
 	}
 
-	private static int CountFaces(int[][] droplets, bool ignoreHollowAreas = false)
+	private static int CountFaces3D(int[][] droplets, bool isPart2 = false)
+	{
+
+		Point3D[] points = droplets.Select(d => new Point3D(X: d[0], Y: d[1], Z: d[2])).ToArray();
+
+		if (!isPart2)
+		{
+			return points.SelectMany(d => GetPossibleNeigbors(d)).Where(n => !points.Contains(n)).Count();
+		}
+		else
+		{
+
+			return GetVisible2(points);
+
+
+		}
+	}
+
+	private static IEnumerable<Point3D> GetPossibleNeigbors(Point3D d)
+	{
+		return new[] { -1, 1 }.Select(o => new[] {	new Point3D(d.X + o,   d.Y,        d.Z),
+													new Point3D(d.X,       d.Y + o,    d.Z),
+													new Point3D(d.X,       d.Y,        d.Z + o)
+												}).SelectMany(p => p);
+	}
+
+	private static int GetVisible2(Point3D[] spread)
+	{
+		int result = 0;
+		Boundaries boundaries = GetBoundaries(spread);
+
+		Queue<Point3D> queue = new();
+		HashSet<Point3D> visited = new();
+
+		Point3D start = new Point3D(0, 0, 0);
+		queue.Enqueue(start);
+
+		while (queue.Count > 0)
+		{
+			Point3D point = queue.Dequeue();
+
+			if (!visited.Add(point)) continue;
+
+			foreach(Point3D neighbor in GetPossibleNeigbors(point))
+			{
+				if (spread.Contains(neighbor))
+				{
+					result++;
+					continue;
+				}
+				if (boundaries.Contains(neighbor))
+				{
+					queue.Enqueue(neighbor);
+				}
+			}
+			
+		}
+
+		return result;
+	}
+
+	public static Boundaries GetBoundaries(Point3D[] matrix)
+	{
+		int xMax = matrix.Select(d => d.X).Max();
+		int xMin = matrix.Select(d => d.X).Min();
+
+		int yMax = matrix.Select(d => d.Y).Max();
+		int yMin = matrix.Select(d => d.Y).Min();
+
+		int zMax = matrix.Select(d => d.Z).Max();
+		int zMin = matrix.Select(d => d.Z).Min();
+
+		return new Boundaries(xMin, xMax, yMin, yMax, zMin, zMax);
+
+	}
+
+
+	public record Boundaries(int xMin, int xMax, int yMin, int yMax, int zMin, int zMax)
+	{
+		public bool Contains(Point3D point)
+		{
+			return (xMin - 1 <= point.X && point.X <= xMax + 1) &&
+				   (yMin - 1 <= point.Y && point.Y <= yMax + 1) &&
+				   (zMin - 1 <= point.Z && point.Z <= zMax + 1);
+		}
+	}
+
+	public record Point3D(int X, int Y, int Z);
+
+
+
+
+	// My first approach in 2D... part 1 works for testdata and input data, part2 only working for testdata  -.-
+
+
+	private static int CountEdges(int[][] droplets, bool onlyOutside = false)
 	{
 		int total = 0;
 
 		int directions = 3;
 		int dimensionSize = droplets.SelectMany(v => v).Max() + 1;
-		int[][] slices = new int[dimensionSize][];
 
-		for (int dir = 0; dir < directions; dir++)
+		for (int dir = 0; dir <= directions-1 ; dir++)
 		{
+			int[][] slices = new int[dimensionSize][];
+			bool countedFirstSliceEdgesInTwoDirections = false;
 			for (int dim = 0; dim < dimensionSize; dim++)
 			{
+				int[][] sliceDroplets = droplets.Where(d => d[(dir) % directions] == dim).ToArray();
 
-				int[][] sliceDroplets = droplets.Where(d => d[dir] == dim).ToArray();
-
-				int[] slize = new int[dimensionSize];
+				int[] slice = new int[dimensionSize];
 
 				foreach (int[] droplet in sliceDroplets)
 				{
 					int x = droplet[(dir + 1) % directions];
 					int y = droplet[(dir + 2) % directions];
-					slize[y] = slize[y] | (1 << x);
+					slice[y] = slice[y] | (1 << x);
 				}
-				slices[dim] = slize;
-			}
-
-			//for (int d = 0; d < slices.Length; d++)
-			//{
-			//	Visualize(slices[d], 20);
-			//	Console.WriteLine($"Z={d}");
-			//}
-
-
-			int countD = 0;
-			for (int d = 0; d < slices.Length; d++)
-			{
-				int[] current = slices[d];
-				if (d == 0)
+				if (onlyOutside)
 				{
-					// check the first slice
-
-					int[] behind = slices[d + 1];
-
-					// count all faces of this layer because they all will be visible from this side
-					countD += CountVisible(current);
-
-					// check if there are faces visible from behind and count them
-					int[] checkBehind = XOr(And(current, behind), current);
-					countD += CountVisible(checkBehind);
-
-				}
-				else if (d > 0 && d < slices.Length - 1)
-				{
-					// the object between the first and the last slice
-
-					int[] before = slices[d - 1];
-					int[] behind = slices[d + 1];
-
-					// count the faces that are not covered from the layer before
-					int[] checkBefore = XOr(And(current, behind), current);
-					countD += CountVisible(checkBefore);
-
-					// count the faces that are not covered from the layer before
-					int[] checkBehind = XOr(And(current, behind), current);
-					countD += CountVisible(checkBehind);
+					slices[dim] = Not(FloodFill(slice));
 				}
 				else
 				{
-					// check the last slice
-					int[] before = slices[d - 1];
-
-					// check if there are faces visible from before and count them
-					int[] checkBehind = XOr(And(current, before), current);
-					countD += CountVisible(checkBehind);
-
-					// count all faces because they are all visible from behind
-					countD += CountVisible(current);
+					slices[dim] = slice;
 				}
-
-				if(ignoreHollowAreas && d < slices.Length-1)
-				{
-					// prepare the next
-
-					int[] prepare = slices[d+1];
-					int[] not = Not(prepare);
-					int[] notOrCurrent =  Or(not, current);
-					int[] notNotAndCurrent = Not(notOrCurrent);
-					prepare = Or(current, notNotAndCurrent);
-
-					slices[d + 1] = prepare;
-				}
-
-				Visualize(current, fps: 5, clear: true) ;
 			}
 
-			Console.WriteLine($"Counted in {(dir == 0 ? "X" : dir == 1 ? "Y" : "Z")}-dimension: {countD}");
+			//slices = TrimFrontAndBack(slices);
+
+			
+			
+			int countD = slices.Select(s => CountHorizontalVisibleSides(s)).Sum();
+
+
+			Console.WriteLine($"Counted in dimension-{dir + 1}: {countD}");
+
 			total += countD;
 		}
-
 		return total;
+	}
+
+	public static int[] FloodFill(int[] slice)
+	{
+		int[] sliceCopy = (int[])slice.Clone();
+		int[] outside = new int[sliceCopy.Length];
+
+		int sizeX = sizeof(int) * 8;
+		int sizeY = slice.Length;
+
+		Queue<(int X, int Y)> queue = new();
+		HashSet<(int X, int Y)> visited = new();
+		// start filling from all the corners
+		queue.Enqueue((0, 0));
+		queue.Enqueue((sizeX - 1, 0));
+		queue.Enqueue((0, sizeY - 1));
+		queue.Enqueue((sizeX - 1, sizeY - 1));
+
+
+
+		while (queue.Count > 0)
+		{
+
+			(int X, int Y) current = queue.Dequeue();
+
+			if (!visited.Contains(current) && ((sliceCopy[current.Y] >> current.X) & 1) == 0)
+			{
+				if (current.X > 0)
+				{
+					var n = (current.X - 1, current.Y);
+					if (!queue.Contains(n))
+					{
+						queue.Enqueue(n);
+					}
+				}
+
+				if (current.X < sizeX - 1)
+				{
+					var n = (current.X + 1, current.Y);
+
+					if (!queue.Contains(n))
+					{
+						queue.Enqueue(n);
+					}
+				}
+				if (current.Y > 0)
+				{
+					var n = (current.X, current.Y - 1);
+
+					if (!queue.Contains(n))
+					{
+						queue.Enqueue(n);
+					}
+				}
+				if (current.Y < sizeY - 1)
+				{
+					var n = (current.X, current.Y + 1);
+
+					if (!queue.Contains(n))
+					{
+						queue.Enqueue(n);
+					}
+				}
+
+				sliceCopy[current.Y] = sliceCopy[current.Y] | (1 << current.X);
+				outside[current.Y] = outside[current.Y] | (1 << current.X);
+
+				visited.Add(current);
+			}
+			//Visualize(outside, fps: 500, true);
+
+		}
+
+		return outside;
+	}
+
+	public static int[][] RemoveEmptySlices(int[][] slices)
+	{
+
+		for (int si = 0; si < slices.Length; si++)
+		{
+			if (slices[si].Sum() == 0)
+			{
+				slices[si] = new int[0];
+			}
+		}
+
+		return slices.Where(s => s.Length != 0).ToArray();
 	}
 
 	public static void Visualize(int[] map, int? fps = null, bool clear = false)
 	{
-		if(fps != null) Thread.Sleep(1000 / fps.Value);
-		if(clear) Console.Clear();
+		if (fps != null) Thread.Sleep(1000 / fps.Value);
+		if (clear) Console.Clear();
 		Console.WriteLine("▲ Y ");
-		for (int y = map.Length - 1; y >= 0 ; y--)
+		for (int y = map.Length - 1; y >= 0; y--)
+		//for (int y = map.Length - 1; y >= 0 ; y--)
 		{
 			string row = Convert.ToString(map[y], 2);
 			row = row.Replace('1', '█').Replace('0', ' ');
-			row = row.PadLeft(map.Length, ' ');
+			row = row.PadLeft(sizeof(int) * 8, ' ');
 			Console.WriteLine($"│{row}");
 		}
-		Console.WriteLine($"└{string.Concat(Enumerable.Repeat('─', map.Length))}► X");
+		Console.WriteLine($"└{string.Concat(Enumerable.Repeat('─', sizeof(int) * 8))}► X");
 	}
 
 	public static int[] And(int[] a, int[] b)
 	{
-		return ArrayOperations(a, b, (va, vb) => va & vb);
+		return ArrayOperation(a, b, (va, vb) => va & vb);
 	}
 
 	public static int[] Or(int[] a, int[] b)
 	{
-		return ArrayOperations(a, b, (va, vb) => va | vb);
+		return ArrayOperation(a, b, (va, vb) => va | vb);
 	}
 
 	public static int[] XOr(int[] a, int[] b)
 	{
-		return ArrayOperations(a, b, (va, vb) => va ^ vb);
+		return ArrayOperation(a, b, (va, vb) => va ^ vb);
 	}
 
 	public static int[] Not(int[] a)
 	{
-		return ArrayOperations(a, (va) => ~va);
+		return ArrayOperation(a, (va) => ~va);
 	}
 
-	public static int[] ArrayOperations(int[] a, Func<int, int> operation)
+	public static int[] ArrayOperation(int[] a, Func<int, int> operation)
 	{
 		int[] result = new int[a.Length];
 		for (int i = 0; i < a.Length; i++)
@@ -192,7 +336,7 @@ public class Day18
 		return result;
 	}
 
-	public static int[] ArrayOperations(int[] a, int[] b, Func<int, int, int> operation)
+	public static int[] ArrayOperation(int[] a, int[] b, Func<int, int, int> operation)
 	{
 		if (a.Length != b.Length)
 		{
@@ -202,7 +346,7 @@ public class Day18
 		int[] result = new int[a.Length];
 		for (int i = 0; i < a.Length; i++)
 		{
-			result[i] = operation(a[i],b[i]);
+			result[i] = operation(a[i], b[i]);
 		}
 
 		return result;
@@ -211,13 +355,13 @@ public class Day18
 	public static int CountVisible(int[] slice)
 	{
 		int count = 0;
-		for(int i = 0; i < slice.Length; i++)
+		for (int i = 0; i < slice.Length; i++)
 		{
 			int current = slice[i];
 
-			while(current > 0 )
+			while (current > 0)
 			{
-				if((current & 1) == 1)
+				if ((current & 1) == 1)
 				{
 					count++;
 				}
@@ -225,5 +369,79 @@ public class Day18
 			}
 		}
 		return count;
+	}
+
+	public static int CountHorizontalVisibleSides(int[] slice)
+	{
+		int sizeY = slice.Length; ;
+		int sizeX = sizeof(int) * 8;
+
+		int visibleSides = 0;
+		for (int y = 0; y < sizeY; y++)
+		{
+			for (int x = 0; x < sizeX; x++)
+			{
+				if (((slice[y] >> x) & 1) == 0)
+				{
+					continue;
+				}
+
+				if (x > 0 && (((slice[y] >> x - 1) & 1) == 0))
+				{
+					visibleSides++;
+				}
+
+				if (x < sizeX - 1 && (((slice[y] >> x + 1) & 1) == 0))
+				{
+					visibleSides++;
+
+				}
+
+
+				if (x == 0 || x == sizeX - 1)
+				{
+					visibleSides++;
+				}
+
+			}
+		}
+		return visibleSides;
+	}
+
+	public static int CountVerticalVisibleSides(int[] slice)
+	{
+		int sizeY = slice.Length; ;
+		int sizeX = sizeof(int) * 8;
+
+		int visibleSides = 0;
+
+		for (int x = 0; x < sizeX; x++)
+		{
+			for (int y = 0; y < sizeY; y++)
+			{
+				if (((slice[y] >> x) & 1) == 0)
+				{
+					continue;
+				}
+
+				if (y > 0 && (((slice[y - 1] >> x) & 1) == 0))
+				{
+					visibleSides++;
+				}
+
+				if (y < sizeY - 1 && (((slice[y + 1] >> x + 1) & 1) == 0))
+				{
+					visibleSides++;
+				}
+
+
+				if (y == 0 || y == sizeY - 1)
+				{
+					visibleSides++;
+				}
+
+			}
+		}
+		return visibleSides;
 	}
 }
